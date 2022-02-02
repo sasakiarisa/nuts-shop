e6448cdf39b09fe3ad66a9b8945f20b9467cf077
sasakiarisa <2525arip@gmail.com>
2022-02-02 13:33:23 +0900

    inicial commit

---
+++ admin(chap6の中身を置く)/all.php
@@ -0,0 +1,6 @@
+<?php require'../header.php';?>
+<?php
+$pdo=new PDO('mysql:host=localhost;dbname=as_shop;charset=utf8',
+'sasaki_arisa','Asdf3333-');
+?>
+<?php require'../footer.php';?>
---
+++ admin(chap6の中身を置く)/delete-output.php
@@ -0,0 +1,11 @@
+<?php require'../header.php';?>
+<?php
+require 'connect.php';
+$sql=$pdo->prepare('delete from product where id=?');
+if($sql->execute([$_REQUEST['id']])){
+  echo '削除に成功しました。';
+}else {
+echo '削除に失敗しました。';
+}
+?>
+<?php require'../footer.php';?>
