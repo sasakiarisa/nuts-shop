e9e1af78f17744be1c56f4c14966e60b37025086
sasakiarisa <2525arip@gmail.com>
2022-03-08 13:11:05 +0900

    www

--- header.php
+++ header.php
@@ -1,25 +1,13 @@
-<<<<<<< HEAD
-<!DOCTYPE html>
-=======
 <!DOCTYPE html> 
->>>>>>> shoke
 <html>
 <head>
 <meta charset="UTF-8">
   <!--ビューポートは header.php に移動 -->
-<<<<<<< HEAD
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-=======
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
->>>>>>> shoke
 <title>PHP Sample Programs</title>
 
 <link rel="stylesheet" href="../style.css">
 
 </head>  <!-- ここまでがページの仕様 映らない-->
-<<<<<<< HEAD
-<body> <!--ここから下が映る-->
-=======
 <body> <!--ここから下が映る-->
 
->>>>>>> shoke
